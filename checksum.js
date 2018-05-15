d3dcc79604beb43766cae246e5db644ebba5611c
tc-imba <liuyh615@sjtu.edu.cn>
2018-05-16 01:11:17 +0800

    file size too large

--- checksum.js
+++ checksum.js
@@ -12,30 +12,40 @@ try {
 
 const data = {};
 
-function calculate(dir) {
+async function calculate(dir) {
   const dir_path = path.join('./public', dir);
   const results = fs.readdirSync(dir_path);
-  results.forEach(name => {
+  for (let i = 0; i < results.length; i++) {
+    const name = results[i];
     const absolute_path = path.join(dir_path, name);
     const file_path = path.join(dir, name);
     const stats = fs.statSync(absolute_path);
     if (stats.isDirectory()) {
-      calculate(file_path);
+      await calculate(file_path);
     } else if (stats.isFile()) {
-      const file = fs.readFileSync(path.join('./public', file_path));
-      const md5 = crypto.createHash('md5');
-      const sha1 = crypto.createHash('sha1');
-      md5.update(file);
-      sha1.update(file);
-      data[file_path] = {
-        size: filesize(stats.size),
-        md5: md5.digest('hex'),
-        sha1: sha1.digest('hex'),
-      };
+      await new Promise((resolve, reject) => {
+        const rs = fs.createReadStream(path.join('./public', file_path));
+        const md5 = crypto.createHash('md5');
+        const sha1 = crypto.createHash('sha1');
+        rs.on('data', data => {
+          md5.update(data);
+          sha1.update(data);
+        });
+        rs.on('end', function() {
+          data[file_path] = {
+            size: filesize(stats.size),
+            md5: md5.digest('hex'),
+            sha1: sha1.digest('hex'),
+          };
+          resolve();
+        });
+      });
     }
-  });
+  }
 }
 
-calculate('/');
-fs.writeFileSync('checksum.json', JSON.stringify(data, null, 4));
+setTimeout(async () => {
+  await calculate('/');
+  fs.writeFileSync('checksum.json', JSON.stringify(data, null, 4));
+}, 0);
 
