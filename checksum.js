6daad3b4bb442b7577cb3bfe4a4be0c497c6ec80
tc-imba <liuyh615@sjtu.edu.cn>
2019-09-15 11:34:19 +0800

    add progress

--- checksum.js
+++ checksum.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const crypto = require('crypto');
 const path = require('path');
 const filesize = require('filesize');
+const ProgressBar = require('progress');
+
 const public_dir = 'public';
 
 try {
@@ -10,7 +12,11 @@ try {
   fs.mkdirSync(public_dir);
 }
 
-const data = {};
+let data = {};
+const result_path = path.join('.', 'checksum.json');
+if (fs.existsSync(result_path)) {
+  data = JSON.parse(fs.readFileSync(result_path, 'utf8'));
+}
 
 async function calculate(dir) {
   const dir_path = path.join('./public', dir);
@@ -23,24 +29,37 @@ async function calculate(dir) {
     if (stats.isDirectory()) {
       await calculate(file_path);
     } else if (stats.isFile()) {
-      await new Promise((resolve, reject) => {
-        const rs = fs.createReadStream(path.join('./public', file_path));
-        const md5 = crypto.createHash('md5');
-        const sha1 = crypto.createHash('sha1');
-        rs.on('data', data => {
-          md5.update(data);
-          sha1.update(data);
+      if (data.hasOwnProperty(file_path) && data[file_path].size === stats.size) {
+        console.log(file_path, 'unchanged');
+      } else {
+        await new Promise((resolve, reject) => {
+          const bar = new ProgressBar(file_path + ' hashing [:bar] :rate/bps :percent :etas', {
+            complete: '=',
+            incomplete: ' ',
+            width: 20,
+            total: stats.size
+          });
+          const rs = fs.createReadStream(path.join('./public', file_path));
+          const md5 = crypto.createHash('md5');
+          const sha1 = crypto.createHash('sha1');
+          rs.on('data', data => {
+            md5.update(data);
+            sha1.update(data);
+            bar.tick(data.length);
+          });
+          rs.on('end', function () {
+            data[file_path] = {
+              size: stats.size,
+              filesize: filesize(stats.size),
+              md5: md5.digest('hex'),
+              sha1: sha1.digest('hex'),
+            };
+            resolve();
+          });
         });
-        rs.on('end', function() {
-          data[file_path] = {
-            size: filesize(stats.size),
-            md5: md5.digest('hex'),
-            sha1: sha1.digest('hex'),
-          };
-          console.log(file_path, data[file_path]);
-          resolve();
-        });
-      });
+      }
+      console.log(data[file_path]);
+      console.log();
     }
   }
 }
